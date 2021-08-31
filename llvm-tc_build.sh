1a744089ab50b4398f7750cb997e272b61ec2f5d
Tomi Prasetio <tomy.prs25@gmail.com>
2021-08-31 12:28:07 +0700

    Get ride

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
+
 git clone https://github.com/bionicverse/proton-clang-build $(pwd)/llvmTC -b master
 cd $(pwd)/llvmTC
 bash build-tc.sh
